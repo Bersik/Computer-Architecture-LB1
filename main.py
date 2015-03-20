f443d7c7856b34cb0260fba64419a5cfee35575c
Bersik <k.sergyk@gmail.com>
2015-03-20 03:24:30 +0200

    new

--- main.py
+++ main.py
@@ -32,15 +32,6 @@ def print_products(products):
         print i.name
         print i.sites
 
-"""def levenstein(s1,s2):
-    n = range(0,len(s1)+1)
-    for y in xrange(1,len(s2)+1):
-        l,n = n,[y]
-        for x in xrange(1,len(s1)+1):
-            n.append(min(l[x]+1,n[-1]+1,l[x-1]+((s2[y-1]!=s1[x-1]) and 1 or 0)))
-    return n[-1]
-"""
-
 def levenstein(a, b):
     n, m = len(a), len(b)
     if n > m:
