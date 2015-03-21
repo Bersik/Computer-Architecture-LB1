d30f352599684d18ad6d62cf6e2d3b75d1f7c6e6
Bersik <k.sergyk@gmail.com>
2015-03-21 05:03:16 +0200

    new

--- tests/test_rozetka.py
+++ tests/test_rozetka.py
@@ -1,9 +1,14 @@
 import unittest
+from rozetka import *
+from site_ import *
 
 class TestFindCountPagesRozetka(unittest.TestCase):
+    path="test_files/"
+
     def test_find_count_pages_rozetka(self):
-        # self.assertEqual(expected, find_count_pages_rozetka(link))
-        assert False # TODO: implement your test here
+        self.assertEqual(57, find_count_pages_rozetka(Site(self.path + "rozetka_test1.html","rozetka.com.ua")))
+        self.assertEqual(1, find_count_pages_rozetka(Site(self.path + "rozetka_test2.html","rozetka.com.ua")))
+        self.assertEqual(3, find_count_pages_rozetka(Site(self.path + "rozetka_test3.html","rozetka.com.ua")))
 
 if __name__ == '__main__':
     unittest.main()
